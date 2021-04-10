6186e894689f71eb29029cebb21d4bb6b2255e79
0x60d94528a98064176e7ba171be8e8e269acf3e18 <0x60d94528a98064176e7ba171be8e8e269acf3e18@nft.life>
2021-04-10 02:21:43 -0400

    * @param permit The MintPermitForAddress signed by the NFT creator

--- contract.sol
+++ contract.sol
@@ -284,3 +284,4 @@ contract NFT1155ContentAddressedLazyMint is
 
     /**
      * @notice Call this function to buy a not yet minted NFT with a permit addressed to yourself
+     * @param permit The MintPermitForAddress signed by the NFT creator
