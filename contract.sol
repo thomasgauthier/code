b3b87e9ee86980f0df8221df263969c8c47d8183
0x64a4d1a91a027a8e1d92bffe8529bc324f17c79d <0x64a4d1a91a027a8e1d92bffe8529bc324f17c79d@nft.life>
2021-04-10 02:21:08 -0400

    require(

--- contract.sol
+++ contract.sol
@@ -263,3 +263,5 @@ contract NFT1155ContentAddressedLazyMint is
             "ERC1155: transfer to the zero address"
         );
         require(!_isMinted[permit.tokenId], "lazy-mint/already-minted");
+
+        require(
