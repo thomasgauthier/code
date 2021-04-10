8143b1f8bb3674efb4f72fea5a0c861d9e4652a0
A A <a@p.ca>
2021-04-10 02:10:37 -0400

    import {

--- contract.sol
+++ contract.sol
@@ -1,1 +1,1 @@
-//SPDX-License-Identifier: UNLICENSEDpragma solidity ^0.7.0;pragma experimental ABIEncoderV2;
+//SPDX-License-Identifier: UNLICENSEDpragma solidity ^0.7.0;pragma experimental ABIEncoderV2;import {
