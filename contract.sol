d1c21722fbb5c8d4fc88767713dfb28b813835cc
0x8bd0e09843c2846d3c5ac92ca3dbf2b68a47df4b <0x8bd0e09843c2846d3c5ac92ca3dbf2b68a47df4b@nft.life>
2021-04-10 02:20:25 -0400

    function isMinted(uint256 tokenId) external view returns (bool minted) {

--- contract.sol
+++ contract.sol
@@ -239,3 +239,4 @@ contract NFT1155ContentAddressedLazyMint is
     /**
      * @notice Check if a NFT has been minted
      */
+    function isMinted(uint256 tokenId) external view returns (bool minted) {
